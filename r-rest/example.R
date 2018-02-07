17e2bea8a159971d20f460286f9fb9e89c5c29dd
Andrew Man Hay Chiu <amhchiu@gmail.com>
2018-02-08 10:18:14 +1100

    Create example.R

---
+++ r-rest/example.R
@@ -0,0 +1,31 @@
+library(httr)
+library(jsonlite)
+
+## I got a free account at coinapi.io
+## Free account provides 100 daily requests free.
+## The next tier allows 1000 daily requests for US$79/month
+
+base     <- "https://rest.coinapi.io/"
+endpoint <- "/v1/exchangerate/BTC?apikey="
+api_key  <- "[your key]"
+
+## Create 
+call <- paste0(base, endpoint, api_key)
+# call
+## Retrieve data via GET call
+get_prices <- GET(call)
+
+## Returns class "response"
+class(get_prices)
+
+## Process into a data table
+get_prices_text <- content(get_prices, "text")  # Convert to "character"
+get_prices_json <- fromJSON(get_prices_text, flatten = TRUE) # Flatten into list
+
+## List contains two objects:
+## 1 - asset_id_base
+## 2 - rates
+
+rates <- get_prices_json$rates
+
+head(rates)
