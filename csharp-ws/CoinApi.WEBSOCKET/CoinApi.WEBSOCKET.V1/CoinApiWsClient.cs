74a9f11423650257d328aa64b5ab8167ac8cceef
mateuszosojca <mateuszosojca@gmail.com>
2019-08-28 15:02:17 +0200

    ws sdk next...

--- csharp-ws/CoinApi.WEBSOCKET/CoinApi.WEBSOCKET.V1/CoinApiWsClient.cs
+++ csharp-ws/CoinApi.WEBSOCKET/CoinApi.WEBSOCKET.V1/CoinApiWsClient.cs
@@ -1,21 +1,29 @@
 ﻿using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
 using CoinApi.WEBSOCKET.V1.DataModels;
 
 namespace CoinApi.WEBSOCKET.V1
 {
     internal class CoinApiWsClient : ICoinApiWsClient, IDisposable
     {
-        private bool _isSandbox;
-        private string _apiKey;
-        private string _url;
-
         private const string SandboxUrl = "wss://ws-sandbox.coinapi.io/";
         private const string NoneSandboxUrl = "wss://ws.coinapi.io/";
 
-        public CoinApiWsClient(bool isSandbox, string apiKey)
+        private bool _isSandbox;
+        private string _apiKey;
+        private string _url;
+        private bool _useAllocation;
+        private readonly ClientWebSocket _client = null;
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+        private readonly QueueThread
+
+        public CoinApiWsClient(bool isSandbox, string apiKey, bool useAllocation)
         {
             _isSandbox = isSandbox;
             _apiKey = apiKey;
+            _useAllocation = useAllocation;
 
             if(_isSandbox)
             {
@@ -29,8 +37,44 @@ namespace CoinApi.WEBSOCKET.V1
 
         public WebsocketState GetWebsocketState => throw new NotImplementedException();
 
-        public void AcceptHelloMessage(Hello helloMessage)
+        public Task AcceptHelloMessage(Hello helloMessage)
         {
+            if(_client == null)
+            {
+                return Task.Run(() => Connect(helloMessage));
+            }
+            //else dorobic reconnect
+
+            return Task.CompletedTask;
+        }
+
+        private async Task Connect(Hello helloMessage)
+        {
+            while (!_cts.IsCancellationRequested)
+            {
+                await HandleConnection();
+            }
+        }
+
+        private async Task ReConnect(Hello helloMessage)
+        {
+            _cts.Cancel();
+        }
+
+        private async Task HandleConnection(Hello helloMessage)
+        {
+            await _client.ConnectAsync(new Uri(_url), _cts.Token);
+            Task.Run(() => DataHandler());
+
+            //send hello message
+        }
+
+        private void DataHandler()
+        {
+            while(!_cts.IsCancellationRequested)
+            {
+
+            }
         }
 
         public void Dispose()
