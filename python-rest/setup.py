df69e018c497782743c3b3e622027bbffb89f20f
Artur Pietrzyk <Svisstack@users.noreply.github.com>
2019-09-04 13:14:34 +0200

    Update setup.py

--- python-rest/setup.py
+++ python-rest/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="coinapi.rest.v1",
-    version="1.0",
+    version="1.1",
     author="Tomasz Przybysz",
     author_email="tom.przybysz@gmail.com",
     description="SDKs for CoinAPI",
