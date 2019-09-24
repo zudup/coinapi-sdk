5bcb21178aeab97b4256863a0529d96fd782840e
Artur Pietrzyk <apietrzyk>
2019-09-24 15:34:48 +0200

    mapping update

--- csharp-rest/CoinAPI.REST.V1/DataModels/SymbolMapping.cs
+++ csharp-rest/CoinAPI.REST.V1/DataModels/SymbolMapping.cs
@@ -8,6 +8,10 @@ namespace CoinAPI.REST.V1.DataModels
     {
         public string symbol_id { get; set; }
         public string symbol_id_exchange { get; set; }
+        public string asset_id_base_exchange { get; set; }
+        public string asset_id_quote_exchange { get; set; }
+        public string asset_id_base { get; set; }
+        public string asset_id_quote { get; set; }
         public decimal? price_precision { get; set; }
         public decimal? size_precision { get; set; }
     }
