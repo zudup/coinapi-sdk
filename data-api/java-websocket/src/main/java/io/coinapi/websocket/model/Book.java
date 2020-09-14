783f3fd5aebd9dd5b3e5542d0a864436ce51271d
Artur Pietrzyk <Svisstack@users.noreply.github.com>
2020-09-14 13:57:39 +0200

    Update Book.java (#74)
    * Update Book.java
    
    bugfix is_snapshot field parsing
    
    * Update pom.xml

--- data-api/java-websocket/src/main/java/io/coinapi/websocket/model/Book.java
+++ data-api/java-websocket/src/main/java/io/coinapi/websocket/model/Book.java
@@ -56,6 +56,7 @@ public class Book extends MessageBase {
         return isSnapshot;
     }
 
+    @JsonAttribute(name = "is_snapshot")
     public void setSnapshot(Boolean snapshot) {
         isSnapshot = snapshot;
     }
