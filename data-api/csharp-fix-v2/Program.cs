94e01d694d27471c28d1fa4b41e09a0b023304f9
Artur Pietrzyk <apietrzyk>
2020-10-09 11:06:04 +0200

    ref

---
+++ data-api/csharp-fix-v2/Program.cs
@@ -0,0 +1,51 @@
+﻿using QuickFix;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COINAPI.FIX.V2
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                QuickFix.SessionSettings settings = new QuickFix.SessionSettings("config_nossl.cfg");
+                MarketDataApp application = new MarketDataApp();
+                QuickFix.IMessageStoreFactory storeFactory = new QuickFix.FileStoreFactory(settings);
+                QuickFix.ILogFactory logFactory = new NullLogFactory(settings);
+                QuickFix.Transport.SocketInitiator initiator = new QuickFix.Transport.SocketInitiator(application, storeFactory, settings, logFactory);
+                initiator.Start();
+
+                System.Threading.Thread.Sleep(Int32.MaxValue);
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.ReadLine();
+            }
+            Environment.Exit(1);
+        }
+
+        public class NullLogFactory : ILogFactory
+        {
+
+            #region LogFactory Members
+
+            public NullLogFactory(SessionSettings settings)
+            { }
+
+            public ILog Create(SessionID sessionID)
+            {
+                return new QuickFix.NullLog();
+            }
+
+            #endregion
+        }
+    }
+}
