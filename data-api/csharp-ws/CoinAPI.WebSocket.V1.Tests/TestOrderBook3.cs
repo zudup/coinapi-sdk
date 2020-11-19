d198d89004f2c6c5274492e18dbbcaef70266009
Artur Pietrzyk <apietrzyk@coinapi.io>
2020-11-19 20:11:56 +0100

    test fix
    Signed-off-by: Artur Pietrzyk <apietrzyk@coinapi.io>

--- data-api/csharp-ws/CoinAPI.WebSocket.V1.Tests/TestOrderBook3.cs
+++ data-api/csharp-ws/CoinAPI.WebSocket.V1.Tests/TestOrderBook3.cs
@@ -19,10 +19,10 @@ namespace CoinAPI.WebSocket.V1.Tests
             {
                 apikey = System.Guid.Parse(config["TestApiKey"]),
                 subscribe_data_type = new string[] { "book_l3" },
-                subscribe_filter_symbol_id = new string[] { "BITSTAMP_SPOT_BTC_USD", "GEMINI_SPOT_BTC_USD COINBASE_SPOT_BTC_USD" }
+                subscribe_filter_symbol_id = new string[] { "BITSTAMP_SPOT_BTC_USD", "GEMINI_SPOT_BTC_USD", "COINBASE_SPOT_BTC_USD" }
             };
 
-            using (var wsClient = new CoinApiWsClient(isSandbox: true))
+            using (var wsClient = new CoinApiWsClient(isSandbox: false))
             {
                 var mre = new ManualResetEvent(false);
                 wsClient.OrderBookL3Event += (s, i) =>
