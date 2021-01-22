f672770b20172e4e843b71fe68992301e5a08eec
Tomasz Przybysz <tom.przybysz@gmail.com>
2021-01-22 10:02:41 +0100

    Update setup.py

--- data-api/python-rest/setup.py
+++ data-api/python-rest/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="coinapi.rest.v1",
-    version="1.1",
+    version="1.2",
     author="Tomasz Przybysz",
     author_email="tom.przybysz@gmail.com",
     description="SDKs for CoinAPI",
